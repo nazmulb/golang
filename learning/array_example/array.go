aa8bb0f79e8d3785b050e7efc3fbaf483c530e16
Nazmul Basher <nazmulbasher@Nazmuls-MacBook-Air.local>
2017-08-25 16:12:41 +0600

    Addded more codes

--- learning/array_example/array.go
+++ learning/array_example/array.go
@@ -6,6 +6,7 @@ func main() {
 	var a [2]string
 
 	 b := [6]int{2, 3, 4, 5, 6, 77}
+	d := []int{6, 78, 44, 5, 7}	
 
 	a[0] = "Hello"
 	a[1] = "World"
@@ -15,4 +16,18 @@ func main() {
 
 	var c []int = b[1:4]
 	fmt.Println(c)
+
+	fmt.Println(d)
+
+	s := []struct {
+		id int
+		name string
+	}{
+		{1, "Nazmul"},
+		{2, "Saiham"},
+		{3, "Abu Sayed"},
+	}
+
+	fmt.Println(s)
+
 }
