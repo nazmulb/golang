bf1ad9af4cb0102a2ed3b1a61a8ee2f4e1e45fe6
Nazmul Basher <nazmulbasher@Nazmuls-MacBook-Air.local>
2017-09-11 13:26:45 +0600

    Added some codes

--- learning/hello/hello.go
+++ learning/hello/hello.go
@@ -3,8 +3,13 @@ package main
 import (
 	"fmt"
 	"github.com/nazmulb/golang/learning/nazmulpkg"
+	"github.com/nazmulb/golang/learning/mathpkg"
 )
 
 func main() {
-	fmt.Printf(nazmulpkg.Show("Hello,"))
+	fmt.Println(nazmulpkg.Show("Hello,"))
+
+	xs := []float64{1,2,3,4}
+	avg := mathpkg.Average(xs)
+	fmt.Println(avg)
 }
